8407f3aa317c78d28dced227ad63b78ecbaa1c7f
Jarrod Stiles <jarrod.stiles@gmail.com>
2025-02-27 08:51:19 -0500

    Replace effect with derived for paging

--- src/lib/components/Pagination.svelte
+++ src/lib/components/Pagination.svelte
@@ -6,24 +6,6 @@
 	};
 
 	let { handlePageChange }: PaginationProps = $props();
-
-	$effect(() => {
-		const pageNumbers = [];
-		const maxVisiblePages = 5;
-
-		let startPage = Math.max(1, paging.currentPage - Math.floor(maxVisiblePages / 2));
-		let endPage = Math.min(paging.totalPages, startPage + maxVisiblePages - 1);
-
-		if (endPage - startPage + 1 < maxVisiblePages) {
-			startPage = Math.max(1, endPage - maxVisiblePages + 1);
-		}
-
-		for (let i = startPage; i <= endPage; i++) {
-			pageNumbers.push(i);
-		}
-
-		paging.pages = pageNumbers;
-	});
 </script>
 
 <div class="pagination-container">
--- src/lib/state/paging.svelte.ts
+++ src/lib/state/paging.svelte.ts
@@ -5,11 +5,29 @@ export type Paging = {
 	pages: number[];
 };
 
+const setPages = (currentPage:number, totalPages:number) => {
+	const pageNumbers: number[] = [];
+	const maxVisiblePages = 5;
+
+	let startPage = Math.max(1, currentPage - Math.floor(maxVisiblePages / 2));
+	const endPage = Math.min(totalPages, startPage + maxVisiblePages - 1);
+
+	if (endPage - startPage + 1 < maxVisiblePages) {
+		startPage = Math.max(1, endPage - maxVisiblePages + 1);
+	}
+
+	for (let i = startPage; i <= endPage; i++) {
+		pageNumbers.push(i);
+	}
+
+	return pageNumbers;
+};
+
 class PagingState {
 	currentPage: number = $state(1);
 	totalPages: number = $state(0);
 	totalItems: number = $state(0);
-	pages: number[] = $state([]);
+	pages: number[] = $derived(setPages(this.currentPage, this.totalPages));
 
 	resetPaging = () => {
 		this.currentPage = 1;
